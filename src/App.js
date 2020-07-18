0e07e354447184aae88f10eeafb4d8d388bec65c
Suhail <SuhailArshad1122@gmail.com>
2020-07-18 23:33:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-//import React, {useRef, useEffect} from 'react';
-import useWebAnimations, {hinge} from "@wellyshen/use-web-animations";
-import './App.css';
+// //import React, {useRef, useEffect} from 'react';
+// import useWebAnimations, {hinge} from "@wellyshen/use-web-animations";
+// import './App.css';
 
 
-function App() {
-  //const element = useRef(null);
-  const { ref, playState, getAnimation } = useWebAnimations({...hinge});
+// function App() {
+//   //const element = useRef(null);
+//   const { ref, playState, getAnimation } = useWebAnimations({...hinge});
     
 
 
 
-  return (
-    <div >
-        <div className="target" ref={ref}>
+//   return (
+//     <div >
+//         <div className="target" ref={ref}>
            
-        </div>
-        current animation state:{playState}
-        <button onClick={()=> getAnimation().pause()}>Pause</button>
-        <button onClick={()=> getAnimation().play()}>Play</button>
-    </div>
-  );
-}
+//         </div>
+//         current animation state:{playState}
+//         <button onClick={()=> getAnimation().pause()}>Pause</button>
+//         <button onClick={()=> getAnimation().play()}>Play</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
